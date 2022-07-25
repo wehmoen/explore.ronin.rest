5a02c62b1f571aa80a97afe94e148d8d07559422
Nico <mail@wehmoen.dev>
2022-07-26 00:16:48 +0200

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Ronin.Rest' });
+});
+
+router.get('/sales', function(req, res, next) {
+  res.render('sales', { title: 'Axie Sales' });
+});
+
+router.get('/transfers', function(req, res, next) {
+  res.render('transfers', { title: 'Axie Transfers' });
+});
+
+
+
+
+
+module.exports = router;
---
+++ twig.js
@@ -0,0 +1,5 @@
+const twig = require('twig');
+
+twig.cache(false);
+
+module.exports = twig.__express;
